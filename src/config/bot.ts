fc9f4444f073bc254312dc129e7807b613b10977
Katlyn Lorimer <katlyn@katlyn.dev>
2020-09-30 16:16:10 -0600

    Refactor bot into dedicated config

---
+++ src/config/bot.ts
@@ -0,0 +1,51 @@
+import { CommandClient } from 'eris'
+
+export const bot = new CommandClient(process.env.TOKEN, {}, {
+  prefix: process.env.PREFIX.split(','),
+  owner: 'theGordHoard#9607'
+})
+
+export const tokenizeArgs = (argumentString: string): string[] => {
+  const args: string[] = []
+
+  let current = ''
+  let openQuote = ''
+  for (const char of argumentString) {
+    if (['"', "'",].includes(char)) {
+      if (current[current.length - 1] === '\\') {
+        current = current.replace(/.$/, char)
+      } else if (openQuote === char) {
+        args.push(current)
+        openQuote = ''
+        current = ''
+      } else if (openQuote === '' && current.length === 0) {
+        openQuote = char
+      } else {
+        current += char
+      }
+    } else if (char === ' ') {
+      if (openQuote === '') {
+        if (current.length > 0) {
+          args.push(current)
+          current = ''
+        }
+      } else {
+        current += char
+      }
+    } else {
+      current += char
+    }
+  }
+
+  if (openQuote !== '') {
+    throw Error('Unmatched quote')
+  }
+
+  if (current.length > 0) {
+    args.push(current)
+  }
+
+  return args
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import 'source-map-support/register'
 import { CommandClient } from 'eris'
 
+import { bot } from './config/bot'
+
 import ascii from './commands/fun/ascii'
 import boyspoints from './commands/fun/boyspoints'
 import canvas from './commands/fun/canvas'
 import evaluate from './commands/util/evaluate'
 import wolfram from './commands/util/wolfram'
-
-const bot = new CommandClient(process.env.TOKEN, {}, {
-  prefix: process.env.PREFIX.split(','),
-  owner: 'theGordHoard#9607'
-})
 
 ascii.init(bot)
 boyspoints.init(bot)
