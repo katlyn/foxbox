14194a22117db3af40d8c577faf6cc22757adbc3
Katlyn Lorimer <katlyn@katlyn.dev>
2020-10-11 00:13:25 -0600

    Require exact number of reactions
    This will prevent repinning if the pin has been removed.

--- src/events/reactionPins.ts
+++ src/events/reactionPins.ts
@@ -10,7 +10,7 @@ const init = (bot: Client): void => {
         .then(async config => {
           if (emoji.name === config.pinReaction) {
             const reacted = await bot.getMessageReaction(msg.channel.id, msg.id, config.pinReaction)
-            if (reacted.length >= config.pinLimit && config.pinLimit !== 0) {
+            if (reacted.length === config.pinLimit && config.pinLimit !== 0) {
               try {
                 await (msg as Message).pin()
               } catch (e) {}
