87bdeb8e7effe95fdb5191b545e39fe0302a76ed
Katlyn Lorimer <katlyn@katlyn.dev>
2020-10-10 23:49:22 -0600

    Allow pinning messages with reactions

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,25 @@ import runMigrations from './config/migrations'
 import pool from './config/postgres'
 
 import ascii from './commands/fun/ascii'
+
+import config from './commands/util/configuration'
 import choose from './commands/util/choose'
 import evaluate from './commands/util/evaluate'
 import wolfram from './commands/util/wolfram'
+
+import reactionPins from './events/reactionPins'
 
 (async () => {
   await runMigrations(pool)
 
   ascii.init(bot)
+
+  config.init(bot)
   choose.init(bot)
   evaluate.init(bot)
   wolfram.init(bot)
+
+  reactionPins.init(bot)
 
   bot.on('ready', () => {
     console.log(`Connected to Discord as ${bot.user.username}#${bot.user.discriminator}`)
